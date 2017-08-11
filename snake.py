97ebf42d113ffbff55782edbff77601ae6804d0d
Szymon Wybranski <swybranski@tableau.com>
2017-08-11 11:38:09 -0700

    fixed dependency on cpu speed

--- snake.py
+++ snake.py
@@ -21,14 +21,17 @@ up = 0
 down = 0
 
 # initial snake (5 rectangles)
-snake = [ pygame.Rect(400+width*0, 300, width, height), 
-          pygame.Rect(400+width*1, 300, width, height), 
+snake = [ pygame.Rect(400+width*0, 300, width, height),
+          pygame.Rect(400+width*1, 300, width, height),
           pygame.Rect(400+width*2, 300, width, height),
           pygame.Rect(400+width*3, 300, width, height),
           pygame.Rect(400+width*4, 300, width, height) ]
-          
+
 food = None
 make_snake_longer = False
+get_current_time = lambda: int(round(time.time() * 1000))
+prev_time = get_current_time()
+time_to_move = True
 
 def move(l=0, r=0, u=0, d=0):
     global left, right, up, down
@@ -36,20 +39,20 @@ def move(l=0, r=0, u=0, d=0):
     right = r
     up = u
     down = d
-    
+
 def wall_hit(head):
     return head.x < 0 or head.x > size[0] or head.y < 0 or head.y > size[1]
- 
+
 def snake_hit_itself(snake):
     seen = set()
     for s in snake:
         if (s.x,s.y) in seen: return True
         seen.update({(s.x,s.y)})
     return False
-    
+
 def snake_hit_food(head):
     return (head.x, head.y) == (food.x, food.y)
-    
+
 def handle_event_loop():
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
@@ -64,14 +67,17 @@ def handle_event_loop():
                 move(d=1)
 
 def move_snake():
-    global snake
+    global snake, make_snake_longer
     head = pygame.Rect.copy(snake[0])
     if left: head.x -= width
     elif right: head.x += width
     elif up: head.y -= height
     elif down: head.y += height
     snake = [head] + snake
-    if not make_snake_longer: del snake[-1]
+    if make_snake_longer:
+        make_snake_longer = False
+    else:
+        del snake[-1]
 
 def place_food():
     global food
@@ -81,39 +87,38 @@ def place_food():
        y = randint(0, size[1]-height)
        y = y - y % height
        food = pygame.Rect(x, y, width, height)
-    
+
 def paint_snake():
     for s in snake:
         pygame.draw.rect(screen, snake_color, s)
-       
+
 def paint_food():
     if food:
         pygame.draw.rect(screen, food_color, food)
 
-while True:
-    handle_event_loop()
+def advance_game():
+    global food, make_snake_longer
     move_snake()
     place_food()
-    
+
     # check food and head collision
     if snake_hit_food(snake[0]):
         food = None
         make_snake_longer = True
-    else:
-        make_snake_longer = False
-    
-    # collision detection 
-    if wall_hit(snake[0]): sys.exit()
-    if snake_hit_itself(snake): sys.exit()
-     
+
+    # check for game over collisions
+    if wall_hit(snake[0]) or snake_hit_itself(snake): sys.exit()
+
+while True:
+    handle_event_loop()
+
+    current_time = get_current_time()
+    if (current_time - prev_time > 50):
+        prev_time = current_time
+        advance_game()
+
     # frame rendering
     screen.fill(bg_color)
     paint_snake()
     paint_food()
     pygame.display.flip()
-
-    # make snake move faster as it grows
-    time.sleep(1/(2*len(snake)))
-    
-    
-    
